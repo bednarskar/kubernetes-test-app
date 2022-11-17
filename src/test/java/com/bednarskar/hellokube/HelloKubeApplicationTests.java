1a04c8d35714c6783d27fd9bc602588d336640bb
bednarskar <>
2022-11-17 17:21:55 +0100

    initial commit

---
+++ src/test/java/com/bednarskar/hellokube/HelloKubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bednarskar.hellokube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
